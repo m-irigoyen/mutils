c890e60f162116bd889347413b068a3552f46ec5
Milan Irigoyen <milan.irigoyen@allegorithmic.com>
2017-07-20 16:37:24 +0200

    init

---
+++ src/collision.cpp
@@ -0,0 +1,18 @@
+#include <mutils\collision.hpp>
+
+namespace mutils
+{
+	bool mutils::isInAABB(Vec2 p, Vec2 aabbTopLeft, Vec2 aabbBottomRight)
+	{
+		// Changing coordinates so that aabtopleft is 0,0
+		aabbBottomRight -= aabbTopLeft;
+		p -= aabbTopLeft;
+		return (p.x >= 0) && (p.y >= 0) && (p.x <= aabbBottomRight.x) && (p.y <= aabbBottomRight.y);
+	}
+
+	bool mutils::isInCircle(Vec2 p, Vec2 center, float radius)
+	{
+		// TODO : assert radius >= 0
+		return (center - p).length() <= radius;
+	}
+}
